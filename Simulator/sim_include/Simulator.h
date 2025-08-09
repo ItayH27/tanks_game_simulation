6fd74b4a8daa0e293a42734bb9bfc1ce90616efd
Maxim <maximg@remitly.com>
2025-08-09 22:28:41 +0300

    Edited readMap function

--- Simulator/sim_include/Simulator.h
+++ Simulator/sim_include/Simulator.h
@@ -5,23 +5,33 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <fstream>
+#include <iostream>
 #include <optional>
+#include "../UserCommon/UC_include/ExtSatelliteView.h"
+
+namespace fs = std::filesystem;
+using std::string, std::ofstream, std::ifstream, std::endl, std::cerr;
 
 class Simulator {
 public:
     virtual ~Simulator() = default;
 
 protected:
-    void readMap(const std::string& file_path);
-    bool extractLineValue(const std::string& line, int& value, const std::string& key, const size_t line_number);
-
     struct MapData {
-        size_t numShells;
-        size_t cols = 0;
-        size_t rows = 0;
+        int numShells;
+        int cols = 0;
+        int rows = 0;
         std::string name;
-        size_t maxSteps;
+        int maxSteps;
+        bool failedInit;
+        ExtSatelliteView* gameBoard = nullptr;
+        ofstream* inputErrors = nullptr;
     };
+
+    MapData readMap(const std::string& file_path);
+    bool extractLineValue(const std::string& line, int& value, const std::string& key, const size_t line_number);
+    bool Simulator::extractValues(Simulator::MapData &mapData, ifstream& inputFile);
 
     std::optional<MapData> map_;
 };
